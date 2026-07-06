975c0b0e0373cb238f95e95ac2f71e4ce8d3c48a
Eduardo Antonino <eduardoguilhermedem987@gmail.com>
2026-07-06 20:01:10 -0300

    feat: Implementações lógicas de questions, welcome, quiz e app

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
 import Welcome from './components/Welcome'
+import Question from './components/Question'
+
+import { useContext } from 'react'
+import { QuizContext } from './context/quiz'
+
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext)
 
   return (
     <>
     <div className='App'>
       <h1>Dev Quiz</h1>
-      <Welcome/>
+      {quizState.gameStage === "Start" && <Welcome/>}
+      {quizState.gameStage === "Playing" && <Question/>}
     </div>
     </>
   )
